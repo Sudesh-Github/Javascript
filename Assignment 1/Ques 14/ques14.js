de61b4209f994df350134f40f82e51c4890bac46
Sudesh-Github <102372827+Sudesh-Github@users.noreply.github.com>
2022-05-16 13:25:20 +0530

    Add files via upload

---
+++ Assignment 1/Ques 14/ques14.js
@@ -0,0 +1,24 @@
+var arr = [];
+
+for(var i=0;i<=100;i++)
+{
+  arr.push(Math.round(Math.random() * 100))
+}
+
+document.write(arr);
+
+console.log(
+Math.max(...arr)
+)
+
+console.log(
+Math.min(...arr)
+)
+
+let evens = arr.filter(arr => arr % 2 ==0)
+
+console.log(evens);
+
+let odds = arr.filter(arr => arr % 2 !=0)
+
+console.log(odds);
