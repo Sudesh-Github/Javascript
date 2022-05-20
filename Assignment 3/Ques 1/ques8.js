6476395dd62ae90e66cf7ee7fd26122262caefcb
Sudesh-Github <102372827+Sudesh-Github@users.noreply.github.com>
2022-05-20 22:24:28 +0530

    Update ques8.js

--- Assignment 3/Ques 1/ques8.js
+++ Assignment 3/Ques 1/ques8.js
@@ -1,21 +1,34 @@
-let Person = {
+/*const myperson = { 
+    fname: 'Sudesh',
+    lname: 'Kamalanathan',
+    age: 23,
+    skills: ['communication', 'Teamwork', 'Adaptation Skills'],
+    address: {
+        City: 'Chennai',
+        pincode: 600126
+    },
+    dateOfBirth:"1998-06-16" ,
+    Married:false,
+    Profession:"software developer"
+};
+const text = JSON.parse(myperson);
+*/
 
-  fname : "Tony",
-  lname : "Stark",
-  age : 30,
-  skills : "Painting",
-
-   Address : {
-
-     city : "California",
-    pincode : 34590
-  },
-  dateofBirth : "24/04/1980",
-  married : new Boolean(" "),
-  profession : "Teacher"
-}
-
+function person(fname,lname,age,skills,dateofbirth,address,married,profession)
+   {
+     this.fname = fname;
+     this.lname = lname;
+     this.age=age;
+     this.skills = skills;
+     this.dateofbirth = dateofbirth;
+     this.address = address;
+     this.married = married;
+     this.profession = profession;
+   }
+let person1=new person("Nikhil","Ramesh",32,["c"],"24/10/1986",{city:"bangalore",pincode:"565485"},"true","sr analyst")
+let person2=new person("Harish","kumar",25,"HTML","08/04/1996",{city:"cochin",pincode:"679038"},"true","jr analyst")
+print=function(person)
 {
-  console.log(Person);
-
+    console.log(person1);
+    console.log(person2);
 }
